17c7bab3b7e85c22ed31cc2a73211b0679901206
FTurleque <fabrice.turleque@gmail.com>
2022-05-16 22:11:35 +0200

    Rename cereals with extend vue js and Stoped in remove cereal function

--- frontend-js/TP2_Employees_v1/assets/js/CreateHTML.js
+++
@@ -1,7 +0,0 @@
-export class CreateHTml {
-    constructor(htmlElementName, className, txt = '') {
-        this.htmlElementName = document.createElement(htmlElementName);
-        this.htmlElementName = className;
-        this.txt = txt;
-    }
-}
